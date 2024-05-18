8e14da4b549afff9df1f24ffb61b3d92316cf907
saharshkanna <saharshkanna@gmail.com>
2024-05-18 14:01:39 -0400

    yay

---
+++ app/src/main/java/com/example/hacksb2024/Track.java
@@ -0,0 +1,42 @@
+package com.example.hacksb2024;
+
+import android.media.MediaPlayer;
+
+public class Track
+{
+
+    //fi
+    String name;
+    MediaPlayer mediaPlayer;
+    int duration;
+    public Track(String n, MediaPlayer mP)
+    {
+        name = n;
+        mediaPlayer = mP;
+        duration = mP.getDuration();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    public void setMediaPlayer(MediaPlayer mediaPlayer) {
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
