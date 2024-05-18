edeb12c5f6a6951b3b5fbaeffbf2c27733f06288
reach_tunawwz <findshardulkulkarni@gmail.com>
2024-05-18 11:18:02 -0400

    nigga

--- app/src/main/java/com/example/hacksb2024/MainActivity.java
+++ app/src/main/java/com/example/hacksb2024/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //hello guys
         Log.d("help", "i wanna die");
-        Log.d("we did it guys", "yay");
+        Log.d("we did it guys", "nigga");
     }
 }
